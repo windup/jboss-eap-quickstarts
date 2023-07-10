e85ce72432a12b1a72a0f27ceac0268f9b139630
Thomas Qvarnström <tqvarnst@redhat.com>
2023-07-10 09:33:33 +0200

    Removed producer for EntityManager and for the FacesContext, which are provided default in Quarkus. The EntityManager already has a producer in the hibernate-orm extension and FacesContext has a producer in the myfaces-quarkus extension

--- kitchensink/src/main/java/org/jboss/as/quickstarts/kitchensink/util/Resources.java
+++ kitchensink/src/main/java/org/jboss/as/quickstarts/kitchensink/util/Resources.java
@@ -38,19 +38,10 @@ import jakarta.persistence.PersistenceContext;
  * </pre>
  */
 public class Resources {
-    @Produces
-    @PersistenceContext
-    private EntityManager em;
 
     @Produces
     public Logger produceLog(InjectionPoint injectionPoint) {
         return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
-    }
-
-    @Produces
-    @RequestScoped
-    public FacesContext produceFacesContext() {
-        return FacesContext.getCurrentInstance();
     }
 
 }
